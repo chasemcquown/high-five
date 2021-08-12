02585cd5a152d280d6f367d8b686a51e4d6ca662
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-08-12 18:31:38 -0500

    added image to README and templates to handlebars

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+// const bodyParser = require('body-parser');
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname + 'views')));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const port = 3330;
+app.listen(port);
+console.log(`Listening to server: http://localhost:${port}`);
+
+// Landing page
+app.get('/', (req, res) => {
+	res.render('main');
+});
