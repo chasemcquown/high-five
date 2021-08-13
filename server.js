bac85e40a35d3c1d4be372badf59c51696c9fa17
unknown <fizz.zehra14@gmail.com>
2021-08-13 14:23:32 -0500

    Connection successfully created

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+//app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
