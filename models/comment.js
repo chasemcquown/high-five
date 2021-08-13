19034d85fcc56bb6ebc3901c378ba9f7228a853c
unknown <fizz.zehra14@gmail.com>
2021-08-13 14:32:34 -0500

    Creating Comments model

---
+++ models/comment.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    text: {
+        type: DataTypes.STRING,
+        validate: {
+            len: [3]
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'User',
+            key: 'id'
+        }
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Post',
+            key: 'id'
+        }
+    }
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Comment'
+});
+module.exports = Comment;
