e3b2d74cef92f5145102c21e6f6fd9b845c64362
unknown <fizz.zehra14@gmail.com>
2021-08-14 14:08:01 -0500

    Creating realtionships with everything

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const Comment = require('./comment');
-const Follower=require('./Follower');
+const Relationship=require('./Follower');
 const Interest=require('./interest');
 const Likes=require('./likes');
 const Post=require('./Post');
@@ -35,53 +35,82 @@ Comment.belongsTo(User, {
     onDelete: "cascade"
 });
 
-/*User can like many posts (one to many )*/
+/*User can do one like on each posts (one to one )
+Creates an association between this user and the Likes. 
+The foreign key is added on the likes.*/
 User.hasMany(Likes, {
     foreignKey: 'user_id',
     onDelete: "cascade"
 });
 
-/*creates an association between the likes and the user. 
-The foreign key is added on the likes.*/
 Likes.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: "cascade"
 });
 
 
-/*User can like many posts (one to many )*/
-User.hasMany(Follower, {
+/*creates many to many relation. using a join table.*/
+User.belongsToMany(Interest,{
+    through:UserInterest,
+    foreignKey:'user_id'
+});
+
+/*creates many to many relation. using a join table.*/
+Interest.belongsToMany(User,{
+    through:UserInterest,
+    foreignKey:'interest_id'
+});
+
+/*User can like many posts (one to many 
+User.hasMany(Relationship, {
     foreignKey: 'user_id',
+    as: 'Followed ',
     onDelete: "cascade"
 });
 
 /*creates an association between the follower and the user. 
-The foreign key is added on the follower.*/
+The foreign key is added on the follower.
 Follower.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: "cascade"
-});
+});)*/
 
 /*==============================================*/
 /*================Post Relations================*/
 /*==============================================*/
 
+/*Post can have many likes (1 to many) */
+Post.hasMany(Likes,
+    {
+        foreignKey:'post_id',
+        onDelete: "cascade"
+    }
+);
 
+/*creates an association between the likes and the post. 
+The foreign key is added on the likes.*/
+Likes.belongsTo(Post,
+    {
+        foreignKey:'post_id',
+        onDelete: "cascade"
+    });
 
-/*==============================================*/
-/*================Comments Relations================*/
-/*==============================================*/
-
-
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
-    onDelete: "cascade"
-});
-
-
-
-Post.hasMany(Comment, {
-    foreignKey: 'post_id',
+/*Post can have many comments (1 to many) */
+Post.hasMany(Comment,{
+    foreignKey:'post_id',
     onDelete: "cascade"
 })
+
+/*==============================================*/
+/*================Comments Relations============*/
+/*==============================================*/
+
+/*creates an association between the Comment and the post. 
+The foreign key is added on the Comment.*/
+Comment.belongsTo(Post,
+    {
+        foreignKey:'post_id',
+        onDelete: "cascade"
+    })
+
 module.exports = { User, Post, Comment };
