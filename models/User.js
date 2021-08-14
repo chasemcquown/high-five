3733acd0f71aea6549fd794638dac6c450303520
unknown <fizz.zehra14@gmail.com>
2021-08-14 16:48:49 -0500

    EMAIL ADDED

--- models/User.js
+++ models/User.js
@@ -20,6 +20,11 @@ User.init(
 			allowNull: false,
 			unique: true,
 		},
+		email:{
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+		},
 		password: {
 			type: DataTypes.STRING,
 			allowNull: false,
