5073a3b3999234ea2ba30bc52b5ec81eeb0532f9
chasem <cpmcquown@gmail.com>
2021-08-15 13:56:39 -0700

    updates user routes

--- controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const userRoutes = require('./user-routes.js');
-const commentRoutes = require('./comment-routes');
-
-router.use('/users', userRoutes);
-router.use('/comments', commentRoutes);
-
-module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,7 @@
 // import dependecies
 const router = require('express').Router();
 
-const { User, Post, Comment, Interest } = require('../../models');
+const { User } = require('../../models');
 
 // get all users for main page cards
 router.get('/', (req, res) => {
@@ -53,17 +53,18 @@ router.post('/', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
       username: req.body.username,
-      email: req.body.email,
+    //   email: req.body.email,
       password: req.body.password
     })
       .then(dbUserData => {
-        req.session.save(() => {
-          req.session.user_id = dbUserData.id;
-          req.session.username = dbUserData.username;
-          req.session.loggedIn = true;
+        // req.session.save(() => {
+        //   req.session.user_id = dbUserData.id;
+        //   req.session.username = dbUserData.username;
+        //   req.session.loggedIn = true;
     
-          res.json(dbUserData);
-        });
+        //   res.json(dbUserData);
+        // });
+        res.json(dbUserData);
       })
       .catch(err => {
         console.log(err);
