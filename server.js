9ed946d62ca3b48a798cfe72568999e26dd76f7b
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-08-14 21:58:19 -0500

    trouble with template, will have to recreate needed to push to clean the branch

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const session = require("express-session");
-const exphbs = require("express-handlebars");
-const helpers = require("./utils/helpers");
-app.use(require("./controllers/"));
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+app.use(require('./controllers/'));
 const PORT = process.env.PORT || 3001;
-const sequelize = require("./config/connection");
+const sequelize = require('./config/connection');
 //const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const path = require("path");
+const path = require('path');
 /*
 const sess = {
   secret: "Super secret secret",
@@ -22,11 +22,11 @@ app.use(session(sess));
 */
 // we pass this file to hbs in order to use the functionality within the helpers.js file
 const hbs = exphbs.create({ helpers });
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //TODO: Uncomment section below once seeds and models are complete
 //sequelize.sync({ force: false }).then(() => {
@@ -34,8 +34,8 @@ app.use(express.static(path.join(__dirname, "public")));
 //});
 
 //TODO: Remove port below once models and seeds are complete
-app.listen(PORT, () => console.log("Now listening"));
+app.listen(PORT, () => console.log('Now listening'));
 
-app.get("/", (req, res) => {
-  res.send("hello");
+app.get('/', (req, res) => {
+	res.send('hello');
 });
