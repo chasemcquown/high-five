3dbaf93d4e203047f0b7bde8227be95e8432946a
chasem <cpmcquown@gmail.com>
2021-08-16 13:52:43 -0700

    initial logic for profile-routes

---
+++ controllers/profile-routes.js
@@ -0,0 +1,98 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Like, userInterst } = require('../models');
+const withAuth = require('../utils/auth');
+
+// get user's interests
+router.get('', (req, res) => {
+    userInterst.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: ['id', 'user_id', 'interst_id']
+    })
+})
+
+// get all users posts for their profile page 
+router.get('/', withAuth, (req, res) => {
+  console.log(req.session);
+  console.log('======================');
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'content',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'), 'like_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// add ability for user to edit a post, route them to edit post page 
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+        
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
