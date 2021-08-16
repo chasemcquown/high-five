2c87cdc72fd0695f4f722e937fa09f877d335784
chasem <cpmcquown@gmail.com>
2021-08-16 16:05:26 -0700

    adds additional logic to profile-routes.js

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -1,98 +1,111 @@
+// imports
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Like, userInterst } = require('../models');
+const { Post, Comment, Like, userInterest } = require('../models');
 const withAuth = require('../utils/auth');
 
-// get user's interests
-router.get('', (req, res) => {
-    userInterst.findAll({
-        where: {
-            user_id: req.session.user_id
+// get all user's posts, an inlcude comments and likes for each post
+router.get('/', (req,res) => {
+ Post.findAll({
+     where: {
+        user_id: req.session.user_id
+     },
+     attributes: [
+         'id',
+         'title',
+         'content',
+         'user_id',
+         'created_at',
+         [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'), 'like_count']
+     ],
+     include: [
+        {
+          model: Comment,
+          attributes: ['id', 'text', 'user_id', 'post_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
         },
-        attributes: ['id', 'user_id', 'interst_id']
-    })
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+ })
+ .then(postInfo => {
+    const posts = postInfo.map(post => post.get({ plain: true }));
+    res.render('user-profile', { posts, loggedIn: true });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 })
 
-// get all users posts for their profile page 
-router.get('/', withAuth, (req, res) => {
-  console.log(req.session);
-  console.log('======================');
-  Post.findAll({
-    where: {
-      user_id: req.session.user_id
-    },
-    attributes: [
-      'id',
-      'title',
-      'content',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'), 'like_count']
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: true });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-// add ability for user to edit a post, route them to edit post page 
-router.get('/edit/:id', withAuth, (req, res) => {
-  Post.findByPk(req.params.id, {
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      if (dbPostData) {
-        const post = dbPostData.get({ plain: true });
-        
-        res.render('edit-post', {
-          post,
-          loggedIn: true
+// get one post to allow for editing of a post
+router.get('/edit/:id', (req,res) => {
+    Post.findByPk(req.params.id, {
+        attributes: [
+          'id',
+          'title',
+          'content',
+          'user_id'
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+        .then(postInfo => {
+          if (postInfo) {
+            const post = postInfo.get({ plain: true });
+            
+            res.render('edit-post', {
+              post,
+              loggedIn: true
+            });
+          } else {
+            res.status(404).end();
+          }
+        })
+        .catch(err => {
+          res.status(500).json(err);
         });
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
-});
+})
 
+// get all of user's interests
+router.get('/', (req, res) => {
+    userInterest.findAll({
+        where: {
+            user_id: req.params.user_id
+        },
+        attributes: [
+            'id',
+            'user_id',
+            'interest_id'
+        ]
+    })
+    .then(interestData => {
+        const interests = interestData.map(interest => interest.get({ plain: true }));
+        res.render('user-profile', { interests, loggedIn: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+})
+
+// export router
 module.exports = router;
