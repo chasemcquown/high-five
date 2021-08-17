752bbceb5fe00dab095baa5bedf8e5f3c952c6a6
unknown <fizz.zehra14@gmail.com>
2021-08-17 16:34:51 -0500

    User Interest seeds created

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,7 @@ const seedComments = require("./comment-seeds");
 const seedUsers = require("./user-seeds");
 const seedPosts = require("./post-seeds");
 const seedInterest=require("./interest-seeds")
+const seedUserInterest=require("./userInterest-seeds")
 
 const sequelize = require("../config/connection");
 
@@ -18,6 +19,8 @@ const seedAll = async () => {
   console.log("--------------");
   await seedInterest();
   console.log("--------------");
+await seedUserInterest();
+console.log("--------------");
 
   process.exit(0);
 };
