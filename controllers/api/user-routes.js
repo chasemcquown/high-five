94542097c2ad527ff83c5ae096e9d01c2c40ec83
unknown <fizz.zehra14@gmail.com>
2021-08-17 17:11:03 -0500

    done interest

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -27,8 +27,7 @@ router.get("/:id", (req, res) => {
       {
         // include users interests (interest model)
         model: Interest,
-        attributes: ["Interest_Category"
-        ],
+        attributes: ["Interest_Category"],
       },
       //{
       //     // include how many followers user has (followers model)
