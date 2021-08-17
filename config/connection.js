2b2252d14b8ca89bf18791c4b6b0ffbfde8aaed3
unknown <fizz.zehra14@gmail.com>
2021-08-16 23:40:05 -0500

    trying to fix an error on seeds auto increment is not working

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "localhost",
       dialect: "mysql",
+      omitNull: true,
       dialectOptions: {
         decimalNumbers: true,
       },
