6005da0fda80bbafdbc2e26f189d7fcd3adb56e8
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-08-17 21:06:21 -0500

    corrected user feed routes and user-feed templatte to pull in users info. (peer coding with Denisse, Chase, Maria, Donald and Fizza)

--- controllers/feed-routes.js
+++ controllers/feed-routes.js
@@ -3,57 +3,55 @@ const sequelize = require('../config/connection');
 const { User, userInterest } = require('../models');
 
 // get all users for user-feed page
-router.get('/', (req,res) => {
-    User.findAll({
-        attributes: [
-            'id', 
-            'username'
-        ]
-    })
-    .then(userData => {
-        const posts = userData.map(user => user.get({ plain: true }));
-  
-        res.render('user-feed', {
-          users,
-          loggedIn: req.session.loggedIn
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
+// router.get('/', (req, res) => {
+// 	User.findAll({
+// 		attributes: ['id', 'username'],
+// 	})
+// 		.then((userData) => {
+// 			const users = userData.map((user) => user.get({ plain: true }));
+// 			console.log(users);
+// 			res.render('user-feed', {
+// 				users,
+// 				loggedIn: req.session.loggedIn,
+// 			});
+// 			console.log(users;
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 			res.status(500).json(err);
+// 		});
+// });
 
 // get a single user and route them to the user-profile page
-router.get('/user/:id', (req,res) => {
-    User.findOne({
-        where: {
-            id: req.params.id
-        },
-        include: [
-            {
-                model: userInterst,
-                attributes: ['id', 'user_id', 'interest_id']
-            }
-        ]
-    })
-    .then(userData => {
-        if (!userData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-  
-        const user = userData.get({ plain: true });
-  
-        res.render('user-profile', {
-          user,
-          loggedIn: req.session.loggedIn
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-})
+router.get('/user/:id', (req, res) => {
+	User.findOne({
+		where: {
+			id: req.params.id,
+		},
+		// include: [
+		//     {
+		//         model: userInterst,
+		//         attributes: ['id', 'user_id', 'interest_id']
+		//     }
+		// ]
+	})
+		.then((userData) => {
+			if (!userData) {
+				res.status(404).json({ message: 'No user found with this id' });
+				return;
+			}
+
+			const user = userData.get({ plain: true });
+
+			res.render('user-profile', {
+				user,
+				loggedIn: req.session.loggedIn,
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const { User, userInterest } = require('../models');
 
 // this will render the login page
 router.get('/', (req, res) => {
@@ -7,8 +8,24 @@ router.get('/', (req, res) => {
 });
 
 // routes to user-feed if they have account
+// get all users for user-feed page
 router.get('/user-feed', (req, res) => {
-	res.render('user-feed');
+	User.findAll({
+		attributes: ['id', 'username'],
+	})
+		.then((userData) => {
+			const users = userData.map((user) => user.get({ plain: true }));
+			console.log(users);
+			res.render('user-feed', {
+				users,
+				loggedIn: req.session.loggedIn,
+			});
+			console.log(users);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 // user will be routed to the sign-up page if they choose sign-up
