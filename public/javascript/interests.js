e439d16525604d573658e7921698ca93101f3f79
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-08-19 14:41:42 -0500

    adjusted the user-feed card to appear without better styling

--- public/javascript/interests.js
+++ public/javascript/interests.js
@@ -3,18 +3,18 @@ async function interests() {
 
 	//first fetch everything from a db into a array
 	var context = {
-		interests: [
-			{ item: 'Reading', value: 'Reading' },
-			{ item: 'Eating', value: 'Eating' },
-			{ item: 'Drinking', value: 'Drinking' },
-			{ item: 'Sports', value: 'Sports' },
-			{ item: 'Games', value: 'Games' },
-			{ item: 'Coding', value: 'Coding' },
-			{ item: 'Exercise', value: 'Exercise' },
-			{ item: 'Movies', value: 'Movies' },
-			{ item: 'Travel', value: 'Travel' },
-			{ item: 'Animals', value: 'Animals' },
-		],
+		// interests: [
+		// 	{ item: 'Reading', value: 'Reading' },
+		// 	{ item: 'Eating', value: 'Eating' },
+		// 	{ item: 'Drinking', value: 'Drinking' },
+		// 	{ item: 'Sports', value: 'Sports' },
+		// 	{ item: 'Games', value: 'Games' },
+		// 	{ item: 'Coding', value: 'Coding' },
+		// 	{ item: 'Exercise', value: 'Exercise' },
+		// 	{ item: 'Movies', value: 'Movies' },
+		// 	{ item: 'Travel', value: 'Travel' },
+		// 	{ item: 'Animals', value: 'Animals' },
+		// ],
 	};
 
 	var templateData = template(context);
