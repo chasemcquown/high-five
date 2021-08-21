0d241c1271c4cd4fbffe366500c5d99eb1b1c076
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-08-21 15:50:07 -0500

    modified main handlebars links on nav bar

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,27 +1,27 @@
 async function newFormHandler(event) {
-	event.preventDefault();
+  event.preventDefault();
 
-	const title = document.querySelector('input[name="post-title"]').value;
-	const content = document.querySelector('input[name="content"]').value;
+  const title = document.querySelector('input[name="post-title"]').value;
+  const content = document.querySelector('textarea[name="content"]').value;
 
-	const response = await fetch(`/api/posts`, {
-		method: 'POST',
-		body: JSON.stringify({
-			title,
-			content,
-		}),
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	});
+  const response = await fetch(`/api/posts`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      content,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-	if (response.ok) {
-		document.location.reload();
-	} else {
-		alert(response.statusText);
-	}
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert(response.statusText);
+  }
 }
 
 document
-	.querySelector('.new-post-form')
-	.addEventListener('submit', newFormHandler);
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
