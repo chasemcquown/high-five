44a3bdc3409e1b5abb49515f50d38fd8fb399273
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-08-22 08:44:39 -0500

    adjusting routes and templates to render recent changes to the way we are presenting the data to the user

--- controllers/feed-routes.js
+++ controllers/feed-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Post } = require('../models');
 
 // get all users for user-feed page
 router.get('/', (req, res) => {
@@ -25,6 +25,11 @@ router.get('/user/:id', (req, res) => {
 		where: {
 			id: req.params.id,
 		},
+		include: [
+			{
+				model: Post,
+			},
+		],
 	})
 		.then((userData) => {
 			if (!userData) {
@@ -44,5 +49,90 @@ router.get('/user/:id', (req, res) => {
 			res.status(500).json(err);
 		});
 });
+// router.get('/user/:id', (req, res) => {
+// 	Post.findAll({
+// 		where: {
+// 			user_id: req.params.user_id,
+// 		},
+// 		attributes: [
+// 			'id',
+// 			'title',
+// 			'content',
+// 			'user_id',
+// 			'created_at',
+// 			// [
+// 			//   sequelize.literal(
+// 			//     "(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)"
+// 			//   ),
+// 			//   "like_count",
+// 			// ],
+// 		],
+// 		include: [
+// 			{
+// 				model: Comment,
+// 				attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+// 				include: {
+// 					model: User,
+// 					attributes: ['username'],
+// 				},
+// 			},
+// 			{
+// 				model: User,
+// 				attributes: ['username'],
+// 			},
+// 		],
+// 	})
+// 		.then((postInfo) => {
+// 			const posts = postInfo.map((post) => post.get({ plain: true }));
+// 			res.render('user-profile', { posts, loggedIn: true });
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 			res.status(500).json(err);
+// 		});
+// });
+
+// router.get('/user/:id', (req, res) => {
+// 	Post.findAll({
+// 		where: {
+// 			id: req.params.id,
+// 		},
+// 		attributes: [
+// 			'id',
+// 			'title',
+// 			'content',
+// 			'user_id',
+// 			'created_at',
+// 			// [
+// 			//   sequelize.literal(
+// 			//     "(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)"
+// 			//   ),
+// 			//   "like_count",
+// 			// ],
+// 		],
+// 		// include: [
+// 		// 	{
+// 		// 		model: Post,
+// 		// 	},
+// 		// ],
+// 	})
+// 		.then((userData) => {
+// 			if (!userData) {
+// 				res.status(404).json({ message: 'No user found with this id' });
+// 				return;
+// 			}
+
+// 			const user = userData.get({ plain: true });
+
+// 			res.render('user-profile', {
+// 				user,
+// 				loggedIn: req.session.loggedIn,
+// 			});
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 			res.status(500).json(err);
+// 		});
+// });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,15 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const apiRoutes = require("./api/");
-const homeRoutes = require("./home-routes.js");
-const profileRoutes = require("./profile-routes.js");
-const feedRoutes = require("./feed-routes.js");
-const dashboardRoutes = require("./dashboard-routes.js");
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+const profileRoutes = require('./profile-routes.js');
+const feedRoutes = require('./feed-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
-router.use("/profile", profileRoutes);
-router.use("/feed-routes", feedRoutes);
-router.use("/dashboard", dashboardRoutes);
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/profile', profileRoutes);
+router.use('/feed-routes', feedRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
