910b761d383c00dbef1cf5085734a487cd6b9798
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-10-17 19:56:17 -0500

    changes

--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -1,9 +1,11 @@
 // identify sign up/submit button
 const signupBtn = document.querySelector('#signupBtn');
-
+// identify button wrapper
+const buttonWrapper = document.querySelector('#interestId');
+// create an array to store user's interests
+const usersInterestsArray = [];
 // send user's info to database
 async function signupHandler(event) {
-
 	event.preventDefault();
 	// retrieve username value
 	const username = document.querySelector('#inputUsername3').value.trim();
@@ -19,10 +21,28 @@ async function signupHandler(event) {
 	const interestFive = document.querySelector('#interest5').value.trim();
 
 	// check that username and password values exist... if they do, send their info to the database
-	if (password && email && username && interestOne && interestTwo && interestThree && interestFour && interestFive) {
+	if (
+		password &&
+		email &&
+		username &&
+		interestOne &&
+		interestTwo &&
+		interestThree &&
+		interestFour &&
+		interestFive
+	) {
 		const response = await fetch('/api/user', {
 			method: 'Post',
-			body: JSON.stringify({ username, email, interestOne, interestTwo, interestThree, interestFour, interestFive, password }),
+			body: JSON.stringify({
+				username,
+				email,
+				interestOne,
+				interestTwo,
+				interestThree,
+				interestFour,
+				interestFive,
+				password,
+			}),
 			headers: { 'Content-Type': 'application/json' },
 		});
 		if (response.ok) {
@@ -34,6 +54,48 @@ async function signupHandler(event) {
 	}
 }
 
+// send user's interest to database
+async function handleInterests(event) {
+	event.preventDefault();
+	if (event.target.id !== '#interestId') {
+		usersInterestsArray.push(event.target.id);
+	}
+
+	const interest1 = usersInterestsArray[0];
+	const interest2 = usersInterestsArray[1];
+	const interest3 = usersInterestsArray[2];
+	const interest4 = usersInterestsArray[3];
+	const interest5 = usersInterestsArray[4];
+
+	// check that user's 5 favorite things values exist... if they do, send their 5 favorite things to the database
+	if (
+		usersInterestsArray[0] &&
+		usersInterestsArray[1] &&
+		usersInterestsArray[2] &&
+		usersInterestsArray[3] &&
+		usersInterestsArray[4]
+	) {
+		const response = await fetch('/api/users/interests', {
+			method: 'Post',
+			body: JSON.stringify({
+				interest1,
+				interest2,
+				interest3,
+				interest4,
+				interest5,
+			}),
+			headers: { 'Content-Type': 'application/json' },
+		});
+
+		// check if response is ok.. if it is, route user to the homepage
+		if (response.ok) {
+			console.log('All good!');
+		} else {
+			alert(response.statusText);
+		}
+	}
+}
 // add event listener to sign up button, then execute function to send user info to database
 signupBtn.addEventListener('click', signupHandler);
-
+// add event listener to button wrapper, then execute function to send user's interest to database
+buttonWrapper.addEventListener('click', handleInterests);
